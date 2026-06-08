2a1cd75bb1095c0248c471f280c6a8d21401e717
Donia Ahmed <daljoife@gmail.com>
2026-06-08 19:12:20 +0200

    Delete Password for my database and use your password for access to database

--- src/main/java/com/example/airlinereervationsystem/database/DBConnection.java
+++ src/main/java/com/example/airlinereervationsystem/database/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=AirlineReservationSystem;encrypt=true;trustServerCertificate=true;";
     private static final String USER = "sa";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = ""; // كلمة مرور قاعدة البيانات
 
     public static Connection getConnection() {
         Connection connection = null;

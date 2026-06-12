d18204341aa624b01903b58424210d2d2038ca16
Donia Ahmed <daljoife@gmail.com>
2026-06-12 18:47:41 +0200

    توضيح و تعديل

--- src/main/java/com/example/airlinereervationsystem/models/UserSession.java
+++ src/main/java/com/example/airlinereervationsystem/models/UserSession.java
@@ -1,13 +1,16 @@
 package com.example.airlinereervationsystem.models;
 
-public class UserSession {
+public class UserSession { // كلاس جلسة المستخدم
     private static User loggedInUser;
     private static int selectedFlightId;
 
+    public static void clearSession() { loggedInUser = null; selectedFlightId = 0; }
+
+    // getter , setter
     public static void setLoggedInUser(User user) { loggedInUser = user; }
     public static User getLoggedInUser() { return loggedInUser; }
 
-    public static void clearSession() { loggedInUser = null; selectedFlightId = 0; }
+
 
     public static int getSelectedFlightId() { return selectedFlightId; }
     public static void setSelectedFlightId(int flightId) { selectedFlightId = flightId; }

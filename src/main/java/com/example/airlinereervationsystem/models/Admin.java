4fa3d5a84468c3a94443b59b56cd8f36b2f3562d
Donia Ahmed <daljoife@gmail.com>
2026-06-12 19:09:17 +0200

    توضيح ال Constructor و دالة DisplayDashboardMessage

--- src/main/java/com/example/airlinereervationsystem/models/Admin.java
+++ src/main/java/com/example/airlinereervationsystem/models/Admin.java
@@ -1,7 +1,8 @@
 package com.example.airlinereervationsystem.models;
-
+// كلاس المسؤول admin يقوم بالورثاة من كلاس المستخدم User
 public class Admin extends User {
 
+    // Constructor
     public Admin(int userId, String fullName, String email, String passwordHash) {
         super(userId, fullName, email, passwordHash, "Admin");
     }
